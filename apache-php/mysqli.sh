09344b1b5ae8198e786defb87f26163504628188
ErikSlevin <107082107+ErikSlevin@users.noreply.github.com>
2023-02-28 19:09:43 +0100

    Update mysqli.sh

--- apache-php/mysqli.sh
+++ apache-php/mysqli.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# mysqli-Abhaengigkeiten installieren fuer MariaDB-Connect
-docker-php-ext-install mysqli
+# Das aktuelle Datum und die Uhrzeit
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Überprüfen, ob das mysqli-Erweiterungsmodul bereits aktiviert ist
+if docker exec -it apache bash -c 'php -m | grep -q mysqli'; then
+    echo "${datetime}: Das mysqli-Erweiterungsmodul ist bereits aktiviert."
+else
+    # Installieren des mysqli-Erweiterungsmoduls
+    docker exec -it apache bash -c 'docker-php-ext-install mysqli'
+    docker exec -it apache bash -c 'docker-php-ext-enable mysqli'
+    echo "${datetime}: Das mysqli-Erweiterungsmodul wurde erfolgreich installiert und aktiviert."
+fi
